0370d4cd6a21f75214057da78a114685e4426c70
Wayou <liuwayong@gmail.com>
2014-06-30 01:31:04 +0800

    add �������� :smirk_cat: :

---
+++ README.md
@@ -0,0 +1,8 @@
+精選
+========
+
+a collection of songs that I love, also shows how to sync lyric with HTML5 audio tag
+
+:point_right: [Go and Enjoy](http://wayou.github.io/selected)!
+
+:exclamation: songs used in this project are only for educational purpose. please don't distribute or sell
--- scripts/selected.js
+++ scripts/selected.js
@@ -1,6 +1,6 @@
 /*
  * Selected | a collection of songs that I love
- * v0.1.18
+ * v0.2.0
  * also as a showcase that shows how to sync lyric with the HTML5 audio tag
  * Wayou  Apr 5th,2014
  * view on GitHub:https://github.com/wayou/selected
@@ -21,12 +21,17 @@ var Selected = function() {
 Selected.prototype = {
     constructor: Selected, //fix the prototype chain
     init: function() {
+        //get all songs and add to the playlist
+        this.initialList(this);
+
         var that = this,
             allSongs = this.playlist.children[0].children,
             currentSong, randomSong;
         this.currentIndex = Math.floor(Math.random() * allSongs.length);
         currentSong = allSongs[this.currentIndex];
         randomSong = currentSong.children[0].getAttribute('data-name');
+
+
         //handle playlist
         this.playlist.addEventListener('click', function(e) {
             if (e.target.nodeName.toLowerCase() !== 'a') {
@@ -49,7 +54,7 @@ Selected.prototype = {
         document.getElementById('bg_dark').addEventListener('click', function() {
             document.getElementsByTagName('html')[0].className = 'colorBg';
         });
-         document.getElementById('bg_pic').addEventListener('click', function() {
+        document.getElementById('bg_pic').addEventListener('click', function() {
             document.getElementsByTagName('html')[0].className = 'imageBg';
         });
         //initially start from a random song
@@ -58,6 +63,30 @@ Selected.prototype = {
         };
         currentSong.className = 'current-song';
         this.play(randomSong);
+    },
+    initialList: function(ctx) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open('GET', './scripts/content.json', false);
+        xhttp.onreadystatechange = function() {
+            if (xhttp.status == 200 && xhttp.readyState == 4) {
+                var fragment = document.createDocumentFragment(),
+                    data = JSON.parse(xhttp.responseText).data,
+                    ol = ctx.playlist.getElementsByTagName('ol')[0],
+                    fragment = document.createDocumentFragment();
+
+                data.forEach(function(v, i, a) {
+                    var li = document.createElement('li'),
+                        a = document.createElement('a');
+                    a.href = 'javascript:void(0)';
+                    a.dataset.name = v.lrc_name;
+                    a.textContent = v.song_name + '-' + v.artist;
+                    li.appendChild(a);
+                    fragment.appendChild(li);
+                });
+                ol.appendChild(fragment);
+            }
+        };
+        xhttp.send();
     },
     play: function(songName) {
         var that = this;
@@ -118,14 +147,14 @@ Selected.prototype = {
             request = new XMLHttpRequest();
         request.open('GET', url, true);
         request.responseType = 'text';
-        //fix for the messy code problem for Chinese reference: http://xx.time8.org/php/20101218/ajax-xmlhttprequest.html
+        //fix for the messy code problem for Chinese.  reference: http://xx.time8.org/php/20101218/ajax-xmlhttprequest.html
         //request['overrideMimeType'] && request.overrideMimeType("text/html;charset=gb2312");
         request.onload = function() {
             that.lyric = that.parseLyric(request.response);
             //display lyric to the page
             that.appendLyric(that.lyric);
         };
-        request.onerror =request.onabort= function(e) {
+        request.onerror = request.onabort = function(e) {
             that.lyricContainer.textContent = '!failed to load the lyric :(';
         }
         this.lyricContainer.textContent = 'loading lyric...';
@@ -137,6 +166,10 @@ Selected.prototype = {
             //this regex mathes the time [00.12.78]
             pattern = /\[\d{2}:\d{2}.\d{2}\]/g,
             result = [];
+
+        // Get offset from lyrics
+        var offset = this.getOffset(text);
+
         //exclude the description parts or empty parts of the lyric
         while (!pattern.test(lines[0])) {
             lines = lines.slice(1);
@@ -150,7 +183,7 @@ Selected.prototype = {
             time.forEach(function(v1, i1, a1) {
                 //convert the [min:sec] to secs format then store into result
                 var t = v1.slice(1, -1).split(':');
-                result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value]);
+                result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]) + parseInt(offset) / 1000, value]);
             });
         });
         //sort the result by time
@@ -172,8 +205,26 @@ Selected.prototype = {
             fragment.appendChild(line);
         });
         lyricContainer.appendChild(fragment);
+    },
+    getOffset: function(text) {
+        //Returns offset in miliseconds.
+        var offset = 0;
+        try {
+            // Pattern matches [offset:1000]
+            var offsetPattern = /\[offset:\-?\+?\d+\]/g,
+                // Get only the first match.
+                offset_line = text.match(offsetPattern)[0],
+                // Get the second part of the offset.
+                offset_str = offset_line.split(':')[1];
+            // Convert it to Int.
+            offset = parseInt(offset_str);
+        } catch (err) {
+            //alert("offset error: "+err.message);
+            offset = 0;
+        }
+        return offset;
     }
-}
+};
 //currently not in use
 // Selected.formatTime = function(time) {
 //     var h, m, s;
